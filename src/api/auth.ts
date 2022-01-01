31b331cfc572cda2dd6859731688327a4fee83e6
Yerson Lasso <yersonlasso05@gmail.com>
2022-01-01 12:32:33 -0500

    refresh auth token endpoint integration

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -28,6 +28,21 @@ export async function auth(email:string, password:string, baseApiUri?:string): P
     })
 }
 
+export async function refresh(refreshToken:string, baseApiUri?:string): Promise<{data:AuthenticationTokens | any, status:number}>{
+    let currentApiUri = baseApiUri
+    if(!currentApiUri){
+        currentApiUri = config.apiBaseUrl
+    }
+    currentApiUri += `${config.apiVersionPath}/auth/token/refresh/`
+    return await fetchJson(currentApiUri, {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        method: 'post',
+        body: JSON.stringify({refresh:refreshToken})
+    })
+}
+
 export async function test(){
     return await fetchJson(`https://jsonplaceholder.typicode.com/todos/1`, {
         headers: {
